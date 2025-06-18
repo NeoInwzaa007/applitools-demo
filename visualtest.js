5e13ed68acdae0686600bca61ef80e73de1b5acd
NeoInwzaa007 <chattanong.d@gmail.com>
2025-06-18 12:08:49 +0700

    force login

--- visualtest.js
+++ visualtest.js
@@ -10,12 +10,23 @@ const edge = require('selenium-webdriver/edge');
 
   let driver = new Builder().forBrowser('MicrosoftEdge').setEdgeOptions(options).build();
 
-  try {
-    await driver.get('https://f1std5-demo9009-o2c.thailanderp.com/Sales/SaleOrderCRMND1.aspx'); // WEB PATH
-    await eyes.open(driver, 'My App', 'Homepage Visual Test');
+    try {
+    await driver.get('https://f1std5-demo9009-o2c.thailanderp.com/Sales/SaleOrderCRMND1.aspx');
+    
+    // 1. กรอก Username
+    await driver.findElement(By.id('txtUsername')).sendKeys('YOUR_USERNAME');
+    
+    // 2. กรอก Password
+    await driver.findElement(By.id('txtPassword')).sendKeys('YOUR_PASSWORD');
+
+    await driver.findElement(By.id('btnLogin')).click();
+
+    await driver.wait(until.elementLocated(By.id('menuOrPageAfterLogin')), 10000);
+
+    await eyes.open(driver, 'My App', 'Menu Visual Test');
     console.log("eyes.open OK");
 
-    await eyes.check('Homepage', Target.window());
+    await eyes.check('MenuPage', Target.window());
     await eyes.close();
     console.log("eyes.close OK");
   } catch (e) {
